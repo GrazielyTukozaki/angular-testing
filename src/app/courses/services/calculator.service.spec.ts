3947780268283b52df971d8d10559f65306cf025
GrazielyTukozaki <grazielybasilio@hotmail.com>
2022-12-07 12:21:01 -0300

    using the beforeEach to build a pre common setup

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,20 +1,28 @@
 import { CalculatorService } from "./calculator.service";
-import { LoggerService } from "./logger.service";
+
+let calculator: CalculatorService,
+loggerSpy:any;
+
+/* the beforeEach will be called before each test, and will works like a previus common setup that all the unit tests can use,
+so we don't have to repeat a lot of code and instatiations */
+
+beforeEach(()=>{
+
+//the spy will observe if the method log its called, in this case, we want that be called only once
+ loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
+ calculator = new CalculatorService (loggerSpy);
+});
 
 describe('CalculatorService',()=>{
   it('should add two numbers', ()=> {
-
-    //the spy will observe if the method log its called, in this case, we want that be called only once
-    const logger = jasmine.createSpyObj('LoggerService', ["log"]);
-    const calculator = new CalculatorService (logger);
     const result = calculator.add(2,2);
     expect(result).toBe(4);
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
   it('should subtract two numbers', ()=> {
-    const calculator = new CalculatorService (new LoggerService());
     const result = calculator.subtract(2,2);
     expect(result).toBe(0, "unexpected substraction result");
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 });
 
