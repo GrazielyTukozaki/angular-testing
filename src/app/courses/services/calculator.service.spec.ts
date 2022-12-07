4cfc8bc29a044fd7b881b54085961ad442d559c7
GrazielyTukozaki <grazielybasilio@hotmail.com>
2022-12-07 12:26:00 -0300

    The describe were at wrong place, now it is fixed

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,19 +1,19 @@
 import { CalculatorService } from "./calculator.service";
 
-let calculator: CalculatorService,
-loggerSpy:any;
-
-/* the beforeEach will be called before each test, and will works like a previus common setup that all the unit tests can use,
-so we don't have to repeat a lot of code and instatiations */
-
-beforeEach(()=>{
-
-//the spy will observe if the method log its called, in this case, we want that be called only once
- loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
- calculator = new CalculatorService (loggerSpy);
-});
-
 describe('CalculatorService',()=>{
+  let calculator: CalculatorService,
+  loggerSpy:any;
+
+  /* the beforeEach will be called before each test, and will works like a previus common setup that all the unit tests can use,
+  so we don't have to repeat a lot of code and instatiations */
+
+  beforeEach(()=>{
+
+  //the spy will observe if the method log its called, in this case, we want that be called only once
+   loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
+   calculator = new CalculatorService (loggerSpy);
+  });
+
   it('should add two numbers', ()=> {
     const result = calculator.add(2,2);
     expect(result).toBe(4);
